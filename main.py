21764dec5f581b5332662d8cfd75a5b6ea2bea39
EarlWSC <ocdaoch@wsc.school.nz>
2024-05-09 12:28:28 +1200

    Half way through Quiz v4

--- main.py
+++ main.py
@@ -1,76 +1,107 @@
+Q_FORMAT = ("{}\n A. {} \n B. {} \n C. {} \n D. {}\n Answer Here:")
+Q_RIGHT = ("\nYou're Right!")
+Q_ELIF = ("Make sure to type something next time?" "\nSorry, but you're Wrong!")
+Q_WRONG = ("\nSorry, but you're Wrong!")
+play = "yes".lower()
 score = 0
+
 # Ask for user's name
 
 name = input("what's your name?")
 
 # Greets user and introduces the Quiz
+
 if name == "":
     print("Where's your name lil bro?")
 print("Welcome to the Quiz,", name)
-print("\nThis Quiz is about Formula One.")
-answer = input("Are you ready to take the quiz?").lower()
-if answer == "Yes" or answer == " Yes".lower():
-    print("\nAlright, let's begin!")
-elif answer == "":
-    print("\nYou're emo aren't you? Anyways, let's begin")
+print("\nThis Quiz is about Formula One!!!")
+
+# Asks if they're ready for the Quiz
+question = "Are you ready to take the quiz?"
+answer = input(question).lower()
+
+# Replies to their reply
+if answer == "yes" or answer == " Yes".lower():
+    print("\nAlright that's good!")
 else:
-    print("\nToo bad, we're going to do the quiz anyways")
+    print("\nToo bad!")
 
-# Asks Question 1 
+while True:
+    try:
+        tries = input("\nHow many attempts would you like?\n Choose from 1 - 3 Attempts")
+        tries = int(tries)
+        break
+    except:
+        print("That's not a valid number, try again.")
 
-answer = input("Who has won 7 World Championship Titles?").lower()
+while play == "yes".lower():
+    question_attempts = tries
+    while question_attempts > 0:
+        # Asks Question 1 
+        question = "Who has won 7 World Championship Titles?"
+        answer = input(question).lower()
 
-# Shows Answer 1
+        # Shows Answer 1
 
-if answer == "Lewis Hamilton".lower() or answer == " Lewis Hamilton".lower():
-    print("\nYou're Right!")
-    score+= 5
-elif answer != "Lewis Hamilton".lower() and answer != " Lewis Hamilton".lower():
-    print("Make sure to type something next time?")
-    print("\nSorry, but you're Wrong!")
-else:
-    print("\nSorry, but you're Wrong!")
-print("\nThe Correct Answer was Lewis Hamilton!")
+        if answer == "Lewis Hamilton".lower() or answer == " Lewis Hamilton".lower():
+            print(Q_RIGHT)
+            score+= 5
+        elif answer != "Lewis Hamilton".lower() and answer != " Lewis Hamilton".lower():
+            print (Q_ELIF)
+        else:
+            print (Q_WRONG)
 
-# Asks Question 2 
+        question_attempts -= 1  
+    print("\nThe Correct Answer was Lewis Hamilton!")
 
-answer = input("Who stole Lewis Hamilton's 8th World Championship Title?").lower()
+    while question_attempts > 0:
+        # Asks Question 2 
+        question = "Who stole Lewis Hamilton's 8th World Championship Title?"
+        answer = input(question).lower()
 
-# Shows Answer 2
+        # Shows Answer 2
 
-if answer == "Max Verstappen".lower() or answer == " Max Verstappen".lower():
-    print("\nYou're Right!")
-    score+= 5
-elif answer != "Max Verstappen".lower() and answer != " Max Verstappen".lower():
-    print("Make sure to type something next time?")
-    print("\nSorry, but you're Wrong!")
-else:
-    print("\nSorry, but you're Wrong!")
-print("\nThe Correct Answer was Max Verstappen!")
+        if answer == "Max Verstappen".lower() or answer == " Max Verstappen".lower():
+            print(Q_RIGHT)
+            score+= 5
+        elif answer != "Max Verstappen".lower() and answer != " Max Verstappen".lower():
+            print (Q_ELIF)
+        else:
+            print (Q_WRONG)
+        print("\nThe Correct Answer was Max Verstappen!")
 
-# Asks Question 3 
-question = "How many years has Rookie Fernando Alonso been racing in Formula One?"
-a = "21"
-b = "17"
-c = "20"
-d = "19"
+        # Asks Question 3 
 
-answer = input("{}\n A. {} \n B. {} \n C. {} \n D. {}\n Answer Here:".format(question, a, b, c, d)).lower()
+        question = "How many years has Rookie Fernando Alonso been racing in Formula One?"
+        a = "21"
+        b = "17"
+        c = "20"
+        d = "19"
 
-# Shows Answer 3
-if answer == c or answer == "c".lower() or answer == " c".lower():
-    print("\nYou're right!")
-    score+= 5
-else:
-    print("\nSorry, but you're Wrong!")
-print("\nThe Correct Answer was C. 20 Years")
-# Quiz Ends
+        answer = input(Q_FORMAT.format(question, a, b, c, d)).lower()
 
-print("In total, {} managed to accumalate {} points".format(name, score))
-if score == 15:
-    print ("\nWow! Perfect Score!")
-if score < 10:
-    print ("\nWelp, that's unfortunate, better luck next time!")
-elif score < 15:
-    print ("\nNice, that's a good score bro!")
+        # Shows Answer 3
+
+        if answer == c or answer == "c".lower() or answer == " c".lower():
+            print(Q_RIGHT)
+            score+= 5
+        else:
+            print(Q_WRONG)
+        print("\nThe Correct Answer was C. 20 Years")
+
+        # Quiz Ends
+        print("In total, {} managed to accumalate {} points".format(name, score))
+        if score == 15:
+            print ("\nWow! Perfect Score!")
+        if score < 10:
+            print ("\nWelp, that's unfortunate, better luck next time!")
+        elif score < 15:
+            print ("\nNice, that's a good score!")
+
+        # Asks user if they want to play the Quiz again
+        play = input("Do you want to play again?")
+        if play == "yes".lower():
+            print ("\nAlright, let's do the quiz One more time!")
+    
+# End of Quiz
 print("\n\nThis is the end of the Quiz, hope you enjoyed.")
