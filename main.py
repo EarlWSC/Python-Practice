1fbf7e3d98698ae3ea94c8e90036acf45beb40ef
EarlWSC <ocdaoch@wsc.school.nz>
2024-05-10 12:56:37 +1200

    uh it kinda buggy we gett here doe

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Q_RIGHT = ("\nYou're Right!")
 Q_ELIF = ("Make sure to type something next time?" "\nSorry, but you're Wrong!")
 Q_WRONG = ("\nSorry, but you're Wrong!")
 play = "yes".lower()
-score = 0
+
 
 # Ask for user's name
 
@@ -30,11 +30,14 @@ while True:
     try:
         tries = input("\nHow many attempts would you like?\n Choose from 1 - 3 Attempts")
         tries = int(tries)
+        print("\nOk, you will have {} attempts!".format(tries))
         break
     except:
-        print("That's not a valid number, try again.")
+        print("\nThat's not a valid number, try again.")
+
 
 while play == "yes".lower():
+    score = 0
     question_attempts = tries
     while question_attempts > 0:
         # Asks Question 1 
@@ -46,12 +49,15 @@ while play == "yes".lower():
         if answer == "Lewis Hamilton".lower() or answer == " Lewis Hamilton".lower():
             print(Q_RIGHT)
             score+= 5
+            break
         elif answer != "Lewis Hamilton".lower() and answer != " Lewis Hamilton".lower():
             print (Q_ELIF)
+
         else:
             print (Q_WRONG)
 
-        question_attempts -= 1  
+
+        question_attempts -= 1     
     print("\nThe Correct Answer was Lewis Hamilton!")
 
     while question_attempts > 0:
@@ -64,14 +70,19 @@ while play == "yes".lower():
         if answer == "Max Verstappen".lower() or answer == " Max Verstappen".lower():
             print(Q_RIGHT)
             score+= 5
+            break
         elif answer != "Max Verstappen".lower() and answer != " Max Verstappen".lower():
             print (Q_ELIF)
+
         else:
             print (Q_WRONG)
-        print("\nThe Correct Answer was Max Verstappen!")
+
+
+        question_attempts -= 1  
+    print("\nThe Correct Answer was Max Verstappen!")
 
         # Asks Question 3 
-
+    while question_attempts > 0:
         question = "How many years has Rookie Fernando Alonso been racing in Formula One?"
         a = "21"
         b = "17"
@@ -85,23 +96,33 @@ while play == "yes".lower():
         if answer == c or answer == "c".lower() or answer == " c".lower():
             print(Q_RIGHT)
             score+= 5
+            break
         else:
             print(Q_WRONG)
-        print("\nThe Correct Answer was C. 20 Years")
 
-        # Quiz Ends
-        print("In total, {} managed to accumalate {} points".format(name, score))
-        if score == 15:
-            print ("\nWow! Perfect Score!")
-        if score < 10:
-            print ("\nWelp, that's unfortunate, better luck next time!")
-        elif score < 15:
-            print ("\nNice, that's a good score!")
+            
+        question_attempts -= 1 
+    print("\nThe Correct Answer was C. 20 Years")
 
-        # Asks user if they want to play the Quiz again
-        play = input("Do you want to play again?")
-        if play == "yes".lower():
-            print ("\nAlright, let's do the quiz One more time!")
+
+    # Quiz Ends
+    print("In total, {} managed to accumalate {} points".format(name, score))
+    if score == 15:
+        print ("\nWow! Steffen thinks you're really hot!")
+            
+    if score < 10:
+        print ("\nWelp, Henry will touch you now!")
+            
+    elif score < 15:
+        print ("\nNice, Jackson really likes you!")
+    question_attempts = -1  
+
+     # Asks user if they want to play the Quiz again
+    play = input("Do you want to play again?")
+    if play == "yes".lower():
+        print ("\nAlright, let's do the quiz One more time!")
+    
+
     
 # End of Quiz
 print("\n\nThis is the end of the Quiz, hope you enjoyed.")
