9de05c523f9216722f4a4b16982a91dc86fa4a3f
EarlWSC <ocdaoch@wsc.school.nz>
2024-05-23 12:25:48 +1200

    Version 6 80%

--- top10.py
+++ top10.py
@@ -1,10 +1,12 @@
 import random
+ANSWERS = ["Lexus", "Mercedes", "Jaguar", "Chevrolette", "Mclaren", "Audi", "Aston Martin", "BMW", "Lotus", "Porsche"]
 
 score = 0
+MAX_TURNS = 10
 # ---- FUNCTIONS ----
 
 
-# -- INTRO --
+# -- Intro --
 def intro():
 
     #Name Input
@@ -24,20 +26,46 @@ def intro():
         print("\nAlright! Maybe you'll do better than what you think!")
 
 
-# -- ATTEMPTS --
+# -- Attempts --
 def getAttempts():
         while True:
             Attempts = input("\nHow many attempts would you like to have? ")
             try:
                 Attempts = int(Attempts)
-                if Attempts >= 0:
+                if Attempts >= 0 and Attempts <= MAX_TURNS:
+                     print("Ok, you will have {} Attempts.".format(Attempts))
                      return Attempts
+                elif Attempts >= MAX_TURNS:
+                     print("Too many attempts, choose a number below 10 Attempts!")
                 else:
-                     print("\nPlease choose a positive number of Attempts!")
+                     print("Please choose a positive number of Attempts!")
             except:
                  print("Please choose a number of Attempts!!")
-                
+            
+
+# -- Miscellaneous --
+def scoreadd(number):
+    return number + 5
+def isCorrect(answer, list):
+     if answer in list:
+          return True
+     else:
+          return False
 # ---- MAIN CODE ----
 
 intro()
-getAttempts()
+Attempts = getAttempts()
+# -- Main Game --
+score = 0
+while Attempts > 0: 
+    answer = input("\nName one of the Top 10 most popular car brands based off popularity rankings. ")
+
+    if isCorrect(answer,ANSWERS).lower():
+        print("Nice! That's one of them.")
+        score += 1
+    else:
+        print("Nice try! Try naming a popular sports car brand like Porsche?")
+        Attempts -= 1
+
+print("Good job! You managed to name {}/10 of the brands!".format(score))
+        
